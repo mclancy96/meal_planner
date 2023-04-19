f63dd71329cd90296b8260beff6514fafccd5c39
Mike <mclancy96@gmail.com>
2023-04-19 17:50:03 -0400

    .env

--- config.js
+++ config.js
@@ -2,13 +2,20 @@
  * This file holds the configurations for our database.
  */
 require("dotenv").config();
+const post = require("postgres");
+// import postgres from 'postgres';
 
-const config = {
-  db: {
-    host: process.env.HOST,
-    user: process.env.USERDB,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-  },
-};
-module.exports = config;
+
+const sql = post('postgres://meal_planner_db_user:95Ac2216nUwJLcuqWO5wsI14JRQeIAWs@dpg-ch05ruseoogs2daj4sfg-a.oregon-postgres.render.com/meal_planner_db', {
+
+})
+
+// const sql = {
+//   db: {
+//     host: process.env.HOST,
+//     user: process.env.USERDB,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE,
+//   },
+// };
+module.exports = sql;
