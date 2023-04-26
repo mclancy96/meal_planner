ef072698f110f3aeecaf9d05a510848bb4751100
Mike <mclancy96@gmail.com>
2023-04-26 09:09:56 -0400

    Adds days

--- public/auxiliary.js
+++ public/auxiliary.js
@@ -3,7 +3,7 @@ const getDayList = () => {
     const today = new Date(Date.now());
     today.setHours(0, 0, 0);
     daylist.push(today.toISOString())
-    for (let i = 1; i < 7; i++) {
+    for (let i = 1; i < 30; i++) {
         let tomorrow = new Date(today);
         tomorrow.setDate(today.getDate() + i)
         daylist.push(tomorrow.toISOString());
